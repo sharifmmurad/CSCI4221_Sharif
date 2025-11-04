d7b121245e32ed28a818491e914abca09991e45a
sharifmmurad <sharifmmurad@gmail.com>
2025-11-04 00:49:06 -0500

    Add files via upload

---
+++ _layout.tsx
@@ -0,0 +1,11 @@
+
+import { Tabs } from 'expo-router';
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ headerShown: true }}>
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen name="tasks" options={{ title: 'Tasks' }} />
+    </Tabs>
+  );
+}
